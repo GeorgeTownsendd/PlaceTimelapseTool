a6a5b13118e15927aaff0612697dc050bd374004
george <georget415@gmail.com>
2023-07-23 17:40:27 +1000

    * updated example.gif

--- canvas_analysis.py
+++ canvas_analysis.py
@@ -456,16 +456,16 @@ if __name__ == "__main__":
     #ref_section = ReferenceSection.download_reference_section('https://brown.ee/LMoP1Zmv.json', 'place_2023')
     #event.create_basic_timelapse(ref_section.name, start_time, end_time)
 
-    refname = 'HIExpansion'
+    refname = 'StreamerFlag'
     event = Event("place_2023")
-    #top_left = Coordinate(-868, 155, 'reddit')
-    #event.canvas_images[-1].add_reference_section(refname, top_left, 125, 125)
+    #top_left = Coordinate(-879, 178, 'reddit')
+    #event.canvas_images[-1].add_reference_section(refname, top_left, 125, 50)
 
-    start_time = datetime.utcnow() - timedelta(hours=17.75)
-    end_time = datetime.utcnow() - timedelta(hours=16)
+    #start_time = datetime.utcnow() - timedelta(hours=17.75)
+    #end_time = datetime.utcnow() - timedelta(hours=16)
 
-    #start_time = datetime.utcnow() - timedelta(hours=1)
-    #end_time = datetime.utcnow()# - timedelta(hours=16)
+    start_time = datetime.utcnow() - timedelta(hours=1)
+    end_time = datetime.utcnow()# - timedelta(hours=16)
 
     event.create_basic_timelapse(coordinates_from=refname, start_time=start_time, end_time=end_time)
     #top_left_reddit = (-500, 178)
